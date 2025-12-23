bbe7ad573a193bec1a8d2e09a9939b3c0a785cbf
Navedulhasan <navedulhasan005@gamil.com>
2025-12-23 10:42:32 +0530

    all

---
+++ classwork/tops1.py
@@ -0,0 +1,39 @@
+# print("hello python")
+# my_rollno = 100
+# print(type(my_rollno))
+# my_percentage = 70.5
+# print(type(my_percentage))
+
+# my_RollNo = int(input("Enter a Number:"))
+# print(my_RollNo)
+
+# set = {138,39,3494,259,49}
+
+# print(type(set))
+# print(set)
+
+# a = 10
+# b = 10
+# print(a is b)
+
+# a = "str"
+# b = 10
+# print(a is b)
+
+# a = 11
+
+
+# a//=2
+# print(a)
+# print(True and True)
+# print(False and True)
+# print(False and False)
+
+
+a = [10,20,30]
+b = "abc"
+
+print(10 in a)
+
+
+
