9e992a1ab7c415380ee9951c8dc8d688e578273d
Navedulhasan <navedulhasan005@gamil.com>
2025-12-25 10:15:38 +0530

    class work

---
+++ classwork/case_programe.py
@@ -0,0 +1,20 @@
+a = int(input("Enter a number:"))
+choice = int(input("Enter your operation:"))
+print("1-add,2-minus,3-multipication,4-division")
+b = int(input("Enter a number:"))
+
+match choice:
+    case 1:
+        choice = a+b
+        print("Answer:",choice)
+    case 2:
+        choice = a-b
+        print("Answer:",choice)
+    case 3:
+        choice = a*b
+        print("Answer:",choice)
+    case 4:
+        choice = a/b
+        print("Answer:",choice)
+    case _:
+        print("_______INVALID CHOICE OF OPERATION______")                
