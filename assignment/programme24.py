28a4232557e323226314c5de4bb2afb04a30eb2a
Navedulhasan <navedulhasan005@gamil.com>
2026-01-30 12:24:06 +0530

    assignment

--- assignment/programme24.py
+++ assignment/programme24.py
@@ -1,1 +1,11 @@
 """24)Write a Python function to insert a string in the middle of a string."""
+
+def insert_in_middle(original_str, str_to_insert):
+    str = input("Enter the original string: ")
+    str1 = input("Enter the string to insert: ")
+    mid_index = len(str) // 2
+    new_str = str[:mid_index] + str1 + str[mid_index:]
+    return new_str
+
+result = insert_in_middle("", "")
+print("String after insertion:", result)
