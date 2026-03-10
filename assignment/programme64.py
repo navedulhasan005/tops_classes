b6b724b8c018a2e95a1f7e5a3fc97c24e03154e4
Navedulhasan <navedulhasan005@gamil.com>
2026-03-10 11:29:14 +0530

    assignment

--- assignment/programme64.py
+++ assignment/programme64.py
@@ -1,9 +1,10 @@
 """64)Write a Python function that checks whether a passed string is 
 palindrome or not"""
-
+str = input("Enter a string:")
 def func(str):
-    if str == str.reverse():
+    if str == str[::-1]:
         print("Palindrome")
     else:
         print("Not Palindrome")
 
+func(str)
