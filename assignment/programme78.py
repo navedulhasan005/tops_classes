63ff5c912690d92026a0905f08480b69811071eb
Navedulhasan <navedulhasan005@gamil.com>
2026-03-11 10:49:48 +0530

    assignment

--- assignment/programme78.py
+++ assignment/programme78.py
@@ -1,5 +1,9 @@
 """78)Write a python program to find the longest words."""
 
 with open('demofile.txt', 'r') as file:
-        words = file
-        
+        text = file.read()
+words = text.splitlines()
+# long = max(words, key=len)
+long = max(words)
+print(long)
+print(len(long))      
