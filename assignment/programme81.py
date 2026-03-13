ad2565f7e8a87b001715bc8231df136f02d3fe94
Navedulhasan <navedulhasan005@gamil.com>
2026-03-13 10:51:04 +0530

    change

--- assignment/programme81.py
+++ assignment/programme81.py
@@ -2,7 +2,6 @@
 
 list = ['apple', 'banana', 'cherry', 123, 45.67]
 with open("demofile.txt","w") as file:
-    for item in list:
-        file.write(f"{item}\n")
-print(item)
+    file.write("\n".join(str(item) for item in list))
+    print("List has been written to the file.")
 
