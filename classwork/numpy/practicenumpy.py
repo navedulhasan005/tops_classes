0a0ff1ab8c78b1961c71494a0adc76a63e5c32dd
Navedulhasan <navedulhasan005@gamil.com>
2026-03-23 11:03:01 +0530

    all

---
+++ classwork/numpy/practicenumpy.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+a = np.array([
+                [1,5,7],
+                [9,8,7]
+            ])
+
+b = np.array([
+                [8,4,3],
+                [5,9,1] 
+            ])
+
+# d = np.concatenate((a,b),axis=1)
+# c = np.stack((a,b),axis=1)
+# print(d)
+# print(c)
+
+c = np.sum((a,b),axis=1)
+# c = np.add(a,b)
+print(c)
